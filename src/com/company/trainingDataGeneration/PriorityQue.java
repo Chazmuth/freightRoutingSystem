6ea2efc7370a42b412391c364d5173f3fa0ee273
Chazmuth <51162613+Chazmuth@users.noreply.github.com>
2022-05-23 21:30:44 +0100

    Delete src/com/company/trainingDataGeneration directory

--- src/com/company/trainingDataGeneration/PriorityQue.java
+++
@@ -1,9 +0,0 @@
-package com.company.trainingDataGeneration;
-
-import java.io.Serializable;
-import java.util.AbstractQueue;
-import java.util.PriorityQueue;
-
-public class PriorityQue<Vertex> extends AbstractQueue<Vertex> implements Serializable {
-    PriorityQueue<Vertex> priorityQueue = new PriorityQueue<Vertex>(int initialCapacity));
-}
