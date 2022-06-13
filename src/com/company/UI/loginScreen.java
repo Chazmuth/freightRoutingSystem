aa0a8683fcc1c0aae73dae7bd12150ed3841cf22
Chazmuth <henryjobling@gmail.com>
2022-06-13 10:57:45 +0100

    ver 3.1.1 started on UI Added access database based on database design

--- src/com/company/UI/loginScreen.java
+++ src/com/company/UI/loginScreen.java
@@ -1,6 +1,8 @@
 package com.company.UI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class loginScreen {
     private JTextField usernameEntry;
@@ -8,6 +10,7 @@ public class loginScreen {
     private JPasswordField passwordField;
     private JButton loginButton;
     private JButton registerButton;
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("loginScreen");
@@ -17,7 +20,17 @@ public class loginScreen {
         frame.setVisible(true);
     }
 
-    public loginScreen(){
+    public loginScreen() {
+        loginButton.addActionListener(e -> {
+            if (usernameEntry.getText().equals("username") && (new String(passwordField.getPassword())).equals("password")
+            ) {
+                System.out.println("login successful");
+            }else{
+                System.out.println("username or password incorrect");
+            }
+        });
+
+        //moodle calculator project for code to link to another GUI
 
     }
 
---
+++ src/com/company/UI/registerScreen.java
@@ -0,0 +1,5 @@
+package com.company.UI;
+
+public class registerScreen {
+
+}
